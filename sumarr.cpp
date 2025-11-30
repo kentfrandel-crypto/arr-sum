cd2398f7dfa6634ba4a974c6cca4db47ee627b96
kentfrandel-crypto <kentfrandel@gmail.com>
2025-11-30 11:58:19 -0800

    Add files via upload

---
+++ sumarr.cpp
@@ -0,0 +1,17 @@
+// name kentsop nguelewe frandel
+//department ; software engineering
+//faculty of science
+
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char** argv) {
+	int result;
+	int arr[6]={1,4,12,14,5,7};
+	result=arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5];
+	cout<<"result="<<result;
+	
+	return 0;
+}
